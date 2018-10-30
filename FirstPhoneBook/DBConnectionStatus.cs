9c8a4446e7dcd4b9a196b313ceb566ef29119945
Szymon Ponikiewski <szymon.ponikiewski@skyrise.tech>
2018-10-30 07:26:13 +0100

    DBConnectionStatus class was added.

---
+++ FirstPhoneBook/DBConnectionStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPhoneBook
+{
+    public class DBConnectionStatus
+    {
+        public bool ConnectionSucceed { get; set; }
+        public string ExceptionMessage { get; set; }
+    }
+}

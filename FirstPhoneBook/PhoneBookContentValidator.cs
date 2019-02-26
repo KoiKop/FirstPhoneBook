67a7e0e221a8562ad5f3e2a93dc48cd2962badc0
Szymon Ponikiewski <szymon.ponikiewski@skyrise.tech>
2019-02-26 09:06:58 +0100

    Input fields validations were added. Tests were fixed.

--- FirstPhoneBook/PhoneBookContentValidator.cs
+++ FirstPhoneBook/PhoneBookContentValidator.cs
@@ -6,8 +6,10 @@ namespace FirstPhoneBook
     {
         public PhoneBookContentValidator()
         {
-            RuleFor(phoneBookContent => phoneBookContent.Name).NotNull().MinimumLength(1).MaximumLength(256);
-
+            RuleFor(x => x.Name).NotNull().MinimumLength(1).MaximumLength(255);
+            RuleFor(x => x.Phone).NotNull().MaximumLength(20);
+            RuleFor(x => x.Email).NotNull().EmailAddress().MaximumLength(255);
+            RuleFor(x => x.Address).NotNull().MaximumLength(255);
         }
     }
 }
